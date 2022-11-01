a9f942f3d2d55346feb3396fae914f93c6f78645
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2022-11-01 23:07:33 +0530

    Add files via upload

---
+++ industry.c
@@ -0,0 +1,10 @@
+/////////////////////////////////////////////////////////
+//
+// Function name :    Addition
+// Input :                  Two integers
+// Output :                Integer
+// Description :          It is used to perform addition
+// Author :                Piyush Manohar Khairnar
+// Date :                   05/03/2022
+//
+////////////////////////////////////////////////////////
